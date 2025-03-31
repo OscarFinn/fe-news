937ded48936ce29b53b1a2d539788a75833fcff7
Oscar Finn <52970695+OscarFinn@users.noreply.github.com>
2025-03-31 15:55:43 +0100

    populated article page with more information

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -5,6 +5,7 @@ import { getArticle } from "../api";
 
 export default function ArticlePage() {
   const [article, setArticle] = useState({});
+  console.log(article);
   const [error, setError] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
@@ -30,12 +31,28 @@ export default function ArticlePage() {
     return <p>loading...</p>;
   }
   if (isError) {
-    return <p>{error.msg}</p>;
+    return (
+      <p>
+        {error.status}: {error.response.data.msg}
+      </p>
+    );
   }
+  const fullDate = new Date(article.created_at);
+  const date = fullDate.toLocaleDateString();
+  const time = fullDate.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
   return (
     <section className="article-page">
+      <img src={article.article_img_url} />
       <h2>{article.title}</h2>
+      <p>
+        posted on {date} at {time} by {article.author}
+      </p>
       <p>{article.body}</p>
+      <p>{article.votes}</p>
     </section>
   );
 }
