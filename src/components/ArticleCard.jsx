6765d05e042b9aff5d302b4c527bb3a81aa9f755
Oscar Finn <52970695+OscarFinn@users.noreply.github.com>
2025-03-31 12:26:14 +0100

    added functionality for fetching articles from api and displaying a placeholder article card

---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,8 @@
+export default function ArticleCard(article) {
+  console.log(article, "<-- article in articlecard");
+  return (
+    <section className="article-card">
+      <h3>This is an article card</h3>
+    </section>
+  );
+}
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, useSearchParams, useParams } from "react-router-dom";
+
+import { getArticles } from "../api";
+
+import ArticleCard from "./ArticleCard";
 
 export default function ArticleList() {
   const [articles, setArticles] = useState([]);
+  console.log(articles);
   const topic = useParams();
+
+  useEffect(() => {
+    getArticles().then((articles) => {
+      setArticles(articles);
+    });
+  }, []);
   return (
-    <body>
+    <section className="article-list">
       <h2>This is where the articles will be listed</h2>
-    </body>
+      {articles.map((article) => {
+        return <ArticleCard article={article} />;
+      })}
+    </section>
   );
 }
